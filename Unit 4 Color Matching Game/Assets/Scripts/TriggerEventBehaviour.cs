574159548024c747140e7f0c0c6bed37924170fb
Benjamin Hood <10885066@my.uvu.edu>
2022-08-31 17:12:51 -0600

    FloatData ScritableObject Finished

---
+++ Unit 4 Color Matching Game/Assets/Scripts/TriggerEventBehaviour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class TriggerEventBehaviour : MonoBehaviour
+{
+    public UnityEvent triggerEnterEvent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        triggerEnterEvent.Invoke();
+    }   
+}
